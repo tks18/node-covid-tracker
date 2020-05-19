acef0d33a5b7daaeda73aeac04013863ad686c1d
Shan.tk <tksudharshan@gmail.com>
2020-05-19 15:41:04 +0530

    Full Code Refactoring

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-  
 import React from 'react';
 
-import { Cards, CountryPicker, Charts } from './components';
+import { Cards, CountryPicker, Chart } from './components';
 import { fetchData } from './api/';
 import styles from './App.module.css';
 
@@ -33,7 +32,7 @@ class App extends React.Component {
         <img className={styles.image} src={image} alt="COVID-19" />
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
-        <Charts data={data} country={country} /> 
+        <Chart data={data} country={country} /> 
       </div>
     );
   }
--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -3,7 +3,7 @@ import { NativeSelect, FormControl } from '@material-ui/core';
 
 import { fetchCountries } from '../../api';
 
-import styles from './CountryPicker.modules.css';
+import styles from './CountryPicker.module.css';
 
 const Countries = ({ handleCountryChange }) => {
   const [countries, setCountries] = useState([]);
